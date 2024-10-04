9417636ce7535aa44bca8bf1d113f6271317ff7c
Anderdood <165479251+Anderdood@users.noreply.github.com>
2024-10-04 19:33:59 +0400

    Update App.java

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Wфывфывorsdld2branch!" );
+        System.out.println( "Hello Wфывфывorsdlssd2branch!" );
     }
 }
