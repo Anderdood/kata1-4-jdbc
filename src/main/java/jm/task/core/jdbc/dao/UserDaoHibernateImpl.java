e1f33b2a09b3166c9a40457fbf778cff472b8d87
Andrey <dudinend@gmail.com>
2024-10-06 02:59:21 +0400

    pp1/3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,9 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
 import java.util.List;
-
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
